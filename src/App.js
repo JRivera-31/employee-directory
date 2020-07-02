4f02fdd97cf12c2aa007fcc870a4f449bb37dcc3
Josh R <jriver35@uncc.edu>
2020-07-01 22:52:32 -0400

    added state to table component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import Container from "./components/Container"
+import employees from "./employee.json"
+import AppBar from "./components/Header"
+import Table from "./components/Table"
 
 class App extends Component {
-  
+  state = {
+    employees
+  }
+
+
   render() {
     return(
-      <Container />
+      <div>
+        <AppBar />,
+        {this.state.employees.map(employee => {
+          return < Table height="100vh"
+            id={employee.results.id.name}
+            firstName={employee.results.name.first}
+            lastName={employee.results.name.last}
+            location={employee.results.location.country}
+            phone={employee.results.phone}
+            email={employee.results.email}/>
+        })}
+      </div>
     );
   }
   
--- src/components/Table.js
+++ src/components/Table.js
@@ -7,6 +7,9 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import ContactsIcon from '@material-ui/icons/Contacts';
+import LocationCityIcon from '@material-ui/icons/LocationCity';
+import EmailIcon from '@material-ui/icons/Email';
 
 const useStyles = makeStyles({
   table: {
@@ -14,19 +17,7 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-export default function SimpleTable() {
+export default function SimpleTable(props) {
   const classes = useStyles();
 
   return (
@@ -34,15 +25,14 @@ export default function SimpleTable() {
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell><strong>Employee Name</strong></TableCell> 
+            <TableCell align="right"><LocationCityIcon/> <strong>Location</strong></TableCell>
+            <TableCell align="right"><ContactsIcon /> <strong>Phone Number</strong></TableCell>
+            <TableCell align="right"><EmailIcon /><strong>Email</strong></TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {props.employee.map((row) => (
             <TableRow key={row.name}>
               <TableCell component="th" scope="row">
                 {row.name}
@@ -50,7 +40,6 @@ export default function SimpleTable() {
               <TableCell align="right">{row.calories}</TableCell>
               <TableCell align="right">{row.fat}</TableCell>
               <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
             </TableRow>
           ))}
         </TableBody>
